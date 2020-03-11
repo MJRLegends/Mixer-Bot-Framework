b4c4781789d776e92c150cd0073b67aff912f5ce
MJRLegends <MJRLegends@users.noreply.github.com>
2020-03-11 19:56:43 +0000

    Reconnect System: Added queue type system, added timeout detection for reconnect code incase of failed reconnect to server

--- src/main/java/com/mjr/mjrmixer/MixerReconnectThread.java
+++ src/main/java/com/mjr/mjrmixer/MixerReconnectThread.java
@@ -2,7 +2,7 @@ package com.mjr.mjrmixer;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.*;
 
 public class MixerReconnectThread extends Thread {
 	private CopyOnWriteArrayList<MixerBotBase> mixerBotsChat = new CopyOnWriteArrayList<MixerBotBase>();
@@ -19,36 +19,69 @@ public class MixerReconnectThread extends Thread {
 	public void run() {
 		while (true) {
 			try {
+				ExecutorService threadPool = Executors.newCachedThreadPool();
 				if (mixerBotsChat.size() != 0) {
 					Iterator<MixerBotBase> iterator = mixerBotsChat.iterator();
+					int attempt = 0;
 					while (iterator.hasNext()) {
+						attempt = attempt + 1;
 						MixerBotBase bot = iterator.next();
-						if (bot.getLastReconnectCodeChat() != 1008 || (bot.getLastReconnectCodeChat() == 1008 && bot.getLastReconnectTimeChat() + 30000 <= System.currentTimeMillis())) {
-							boolean done = false;
-							if (bot.isChatConnectionClosed()) {
-								bot.reconnectChat();
-								done = true;
-							}
-							Thread.sleep(mixerBotSleepTime * 1000);
-							if (done && !bot.isChatConnectionClosed())
-								mixerBotsChat.remove(bot);
-						}
+						do {
+							if (bot.getLastReconnectCodeChat() != 1008 || (bot.getLastReconnectCodeChat() == 1008 && bot.getLastReconnectTimeChat() + 30000 <= System.currentTimeMillis())) {
+								if (bot.isChatConnectionClosed()) {
+									Callable<Boolean> callable = () -> {
+										bot.reconnectChat();
+										return true;
+									};
+									Future<Boolean> future = threadPool.submit(callable);
+									try {
+										future.get(30, TimeUnit.SECONDS);
+									} catch (TimeoutException e) {
+										MixerEventHooks.triggerOnInfoEvent(bot.getChannelName(), bot.getChannelID(), "[Mixer Framework Reconnect] Chat reconnect has timed out for taking to long will skip and retry!");
+										if (!bot.isChatConnectionClosed())
+											bot.disconnectChat();
+									}
+								}
+								Thread.sleep((mixerBotSleepTime * 1000) * attempt);
+								if (!bot.isChatConnectionClosed())
+									mixerBotsChat.remove(bot);
+							} else
+								Thread.sleep(5 * 1000);
+						} while (bot.isChatConnectionClosed());
+						attempt = 0;
 					}
 				}
 				if (mixerBotsConstell.size() != 0) {
 					Iterator<MixerBotBase> iterator = mixerBotsConstell.iterator();
+					int attempt = 0;
+
 					while (iterator.hasNext()) {
+						attempt = attempt + 1;
 						MixerBotBase bot = iterator.next();
-						if (bot.getLastReconnectCodeConstel() != 1008 || (bot.getLastReconnectCodeConstel() == 1008 && bot.getLastReconnectTimeConstel() + 30000 <= System.currentTimeMillis())) {
-							boolean done = false;
-							if (bot.isConstellationConnectionClosed()) {
-								bot.reconnectConstellation();
-								done = true;
-							}
-							Thread.sleep(mixerBotSleepTime * 1000);
-							if (done && !bot.isConstellationConnectionClosed())
-								mixerBotsConstell.remove(bot);
-						}
+
+						do {
+							if (bot.getLastReconnectCodeConstel() != 1008 || (bot.getLastReconnectCodeConstel() == 1008 && bot.getLastReconnectTimeConstel() + 30000 <= System.currentTimeMillis())) {
+								if (bot.isConstellationConnectionClosed()) {
+									Callable<Boolean> callable = () -> {
+										bot.reconnectConstellation();
+										return true;
+									};
+									Future<Boolean> future = threadPool.submit(callable);
+									try {
+										future.get(30, TimeUnit.SECONDS);
+									} catch (TimeoutException e) {
+										MixerEventHooks.triggerOnInfoEvent(bot.getChannelName(), bot.getChannelID(), "[Mixer Framework Reconnect] Constellation reconnect has timed out for taking to long will skip and retry!");
+										if (!bot.isConstellationConnectionClosed())
+											bot.disconnectConstellation();
+									}
+								}
+								Thread.sleep((mixerBotSleepTime * 1000) * attempt);
+								if (!bot.isConstellationConnectionClosed())
+									mixerBotsConstell.remove(bot);
+							} else
+								Thread.sleep(5 * 1000);
+						} while (bot.isConstellationConnectionClosed());
+						attempt = 0;
 					}
 				}
 
